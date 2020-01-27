2d7e422c499a24108d5960780dffacab36c6c3ec
amir-silverbot <amir81.home@gmail.com>
2020-01-27 21:42:10 +0330

    1.00

--- caesar-cipher-tool.py
+++ caesar-cipher-tool.py
@@ -1,4 +1,4 @@
-#gathering inputs
+# gathering inputs
 inputString = input("Enter your string: ")
 print("\n")
 stringLength = len(inputString)
@@ -9,12 +9,12 @@ newASCII = 0
 if (str.isdigit(shift) == False):
     shift = 3
     print("Shift has been set to 3 because of bad input...\n")
-#watch out for out of range shifts!
+# watch out for out of range shifts!
 shift = int(shift) % 26
 
 for i in range(stringLength):
     
-    #uppercase characters
+    # uppercase characters
     if (ord(inputString[i]) >= 65 and ord(inputString[i]) <= 90):
         if ((ord(inputString[i]) + shift) > 90):
             newASCII = ((ord(inputString[i]) + shift) % 90) + 64
@@ -22,7 +22,7 @@ for i in range(stringLength):
             newASCII = ord(inputString[i]) + shift
         outputString += chr(newASCII)
                                           
-    #lowercase characters
+    # lowercase characters
     elif (ord(inputString[i]) >= 97 and ord(inputString[i]) <= 122):
         if ((ord(inputString[i]) + shift) > 122):
             newASCII = ((ord(inputString[i]) + shift) % 122) + 96
@@ -30,11 +30,11 @@ for i in range(stringLength):
             newASCII = ord(inputString[i]) + shift
         outputString += chr(newASCII)
 
-    #remaining characters
+    # remaining characters
     else:
         outputString += chr(ord(inputString[i]))
 
-#print outputString
+# print outputString
 print("Encrypted string is: " + outputString)
 print("\n")
 input("Press enter to continue...")
